28994055a25a21c04969cc5eb4b402fb6194394f
Davide Di Stefano <distefano.2149981@studenti.uniroma1.it>
2025-05-07 23:33:57 +0200

    Update PacMan.java
    Javadoc

--- src/scripts/PacMan.java
+++ src/scripts/PacMan.java
@@ -1,97 +1,106 @@
 package scripts;
 
 /**
- * The class {@code PacMan} create and manage a PacMan object and its attributes
- * like position coordinates, its current direction, its spawn coordinates 
- * the class represent the player controlled character,
- * this character must avoid ghosts and eat all food to win.This class manages its movements
- * and teleport 
+ * Represents the Pac-Man character controlled by the player.
+ * The {@code PacMan} class manages Pac-Man's attributes, such as position coordinates,
+ * movement direction, and spawn coordinates. Pac-Man must avoid ghosts and eat all food to win.
+ * This class handles movement, collision detection, and teleportation logic.
  * 
  * @see Character
  * @see Game#keyPressed(java.awt.event.KeyEvent)
  * 
  * @author Davide Di Stefano
- * @version 1.0.0
+ * @version 1.2.0
  * @since 1.0.0
  */
-public class PacMan extends Character{
-	
-	/**
-	 * This builder creates a PacMan object taking in :
-	 * 
-	 * @param coordinatesXY the current pacMan coordinates at start this value is given to {@code defaultCoordinatesXY}
-	 * while {@code coordinatesXY} gets updated when pacman moves 
-	 * @param currentDirectionXY an integer array specifying the pac-man movement direction (x, y) given by the user.
-	 */
-	public PacMan(int[] currentCoordinatesXY, int[] currentDirectionXY) {
-		super(currentCoordinatesXY, currentDirectionXY);
-	}
-	
-	/**
-	 * This method is used to moved pac-man across the map based on the input of user and collisions with walls
-	 * 
-	 * @see Game#keyPressed(java.awt.event.KeyEvent) modify the direction from user input and validate it
-	 * @see PacMan#verifyDirectionUpdate(String[][], int[]) validate the direction of the movement to check if path is free makes the movement feel more fluid
-	 */
-	@Override
-	public void checkCollisionAndMove(String[][] gameBoard) {
-		
-		// Square good events processing
-		String targetTileContent = gameBoard[currentCoordinatesXY[1]+currentDirectionXY[1]][currentCoordinatesXY[0]+currentDirectionXY[0]];
-		if(targetTileContent.contains(".")){
-			Game.foodsScoreIncrease();
-			SoundPlayer.playSound("/Sounds/pacManEating.wav");
-			}
-		if(targetTileContent.contains("x")){
-			Game.increaseInvincibilityTime();
-			SoundPlayer.playSound("/Sounds/powerUpEaten.wav");
-			}
-		if(targetTileContent.contains("f")){Game.increaseLifes();
-			SoundPlayer.playSound("/Sounds/fruitEaten.wav");
-			}
-		
-		// Pac-Man movement
-		if(!targetTileContent.equals("W") && !targetTileContent.equals("0") && !targetTileContent.equals("O")) {
-			gameBoard[currentCoordinatesXY[1]][currentCoordinatesXY[0]] = gameBoard[currentCoordinatesXY[1]][currentCoordinatesXY[0]].replace("P", "");
-			gameBoard[currentCoordinatesXY[1]+currentDirectionXY[1]][currentCoordinatesXY[0]+currentDirectionXY[0]] = "P";
-			if(gameBoard[currentCoordinatesXY[1]][currentCoordinatesXY[0]].length() == 0){
-				gameBoard[currentCoordinatesXY[1]][currentCoordinatesXY[0]] = " ";
-			}
-		currentCoordinatesXY = new int[] {currentCoordinatesXY[0] + currentDirectionXY[0], currentCoordinatesXY[1] + currentDirectionXY[1]};
+public class PacMan extends Character {
 
-		}
-	}
-	
-	/**
-	 * Teleports pac-man to a position. Is used by the portals at sides of the map or to reset its position after a victory or a defeat
-	 */
-	@Override
-	public void teleportAt(String[][] gameBoard, int[] targetCoordinatesXY) {
-		
-		//Pac-man movement
-		gameBoard[currentCoordinatesXY[1]][currentCoordinatesXY[0]] = gameBoard[currentCoordinatesXY[1]][currentCoordinatesXY[0]].replace("P", "");
-		if(gameBoard[currentCoordinatesXY[1]][currentCoordinatesXY[0]].length() == 0){
-			gameBoard[currentCoordinatesXY[1]][currentCoordinatesXY[0]] = " ";
-		}
-		currentCoordinatesXY = targetCoordinatesXY;
-		gameBoard[currentCoordinatesXY[1]][currentCoordinatesXY[0]] = "P";
-	}
-	
+    /**
+     * Constructs a new Pac-Man object.
+     * 
+     * @param currentCoordinatesXY The initial coordinates of Pac-Man. This value is used to set
+     *                             {@code defaultCoordinatesXY}, while {@code coordinatesXY}
+     *                             gets updated as Pac-Man moves.
+     * @param currentDirectionXY   An integer array specifying Pac-Man's initial movement
+     *                             direction (x, y) based on user input.
+     */
+    public PacMan(int[] currentCoordinatesXY, int[] currentDirectionXY) {
+        super(currentCoordinatesXY, currentDirectionXY);
+    }
 
-	/**
-	 * This method is useful to verify if the position where pac-man will be moved by the user input is not a wall
-	 * this function does not handles fully the pac-man collision for this is needed the method {@code checkCollisionAndMove}
-	 *  
-	 * @param gameBoard this 2d Strings array represents the current state of the screen representing walls, ghosts, pac-man, food and else
-	 * @param inputDirectionXY this is the desired direction chosen by user to be tested
-	 * 
-	 * @see #checkCollisionAndMove pacman movement
-	 * @see Game#keyPressed(java.awt.event.KeyEvent) modify the direction from user input and validate it
-	 */
-	public void verifyDirectionUpdate(String[][] gameBoard, int[] inputDirectionXY) {
-		if(!gameBoard[currentCoordinatesXY[1]+inputDirectionXY[1]][currentCoordinatesXY[0]+inputDirectionXY[0]].equals("W")) {
-			updateDirection(inputDirectionXY);	
-		}
-		
-	}	
+    /**
+     * Processes Pac-Man's movement across the game board, based on user input and collision
+     * detection. This method handles interactions with walls, food, power-ups, and other game
+     * elements.
+     * 
+     * @param gameBoard A 2D array representing the current state of the game board, including
+     *                  walls, food, ghosts, and other elements.
+     * 
+     * @see Game#keyPressed(java.awt.event.KeyEvent) Updates the movement direction based on user input.
+     * @see PacMan#verifyDirectionUpdate(String[][], int[]) Validates the movement direction
+     *      and ensures smooth gameplay.
+     */
+    @Override
+    public void checkCollisionAndMove(String[][] gameBoard) {
+        // Square good events processing
+        String targetTileContent = gameBoard[currentCoordinatesXY[1] + currentDirectionXY[1]][currentCoordinatesXY[0] + currentDirectionXY[0]];
+        if (targetTileContent.contains(".")) {
+            Game.foodsScoreIncrease();
+            SoundPlayer.playSound("/Sounds/pacManEating.wav");
+        }
+        if (targetTileContent.contains("x")) {
+            Game.increaseInvincibilityTime();
+            SoundPlayer.playSound("/Sounds/powerUpEaten.wav");
+        }
+        if (targetTileContent.contains("f")) {
+            Game.increaseLifes();
+            SoundPlayer.playSound("/Sounds/fruitEaten.wav");
+        }
+
+        // Pac-Man movement
+        if (!targetTileContent.equals("W") && !targetTileContent.equals("0") && !targetTileContent.equals("O")) {
+            gameBoard[currentCoordinatesXY[1]][currentCoordinatesXY[0]] = gameBoard[currentCoordinatesXY[1]][currentCoordinatesXY[0]].replace("P", "");
+            gameBoard[currentCoordinatesXY[1] + currentDirectionXY[1]][currentCoordinatesXY[0] + currentDirectionXY[0]] = "P";
+            if (gameBoard[currentCoordinatesXY[1]][currentCoordinatesXY[0]].length() == 0) {
+                gameBoard[currentCoordinatesXY[1]][currentCoordinatesXY[0]] = " ";
+            }
+            currentCoordinatesXY = new int[] { currentCoordinatesXY[0] + currentDirectionXY[0], currentCoordinatesXY[1] + currentDirectionXY[1] };
+        }
+    }
+
+    /**
+     * Teleports Pac-Man to a specified position on the game board. This is typically used
+     * for map-side portals or to reset Pac-Man's position after a victory or defeat.
+     * 
+     * @param gameBoard           A 2D array representing the game board's state.
+     * @param targetCoordinatesXY The target coordinates where Pac-Man will be teleported.
+     */
+    @Override
+    public void teleportAt(String[][] gameBoard, int[] targetCoordinatesXY) {
+        // Pac-Man movement
+        gameBoard[currentCoordinatesXY[1]][currentCoordinatesXY[0]] = gameBoard[currentCoordinatesXY[1]][currentCoordinatesXY[0]].replace("P", "");
+        if (gameBoard[currentCoordinatesXY[1]][currentCoordinatesXY[0]].length() == 0) {
+            gameBoard[currentCoordinatesXY[1]][currentCoordinatesXY[0]] = " ";
+        }
+        currentCoordinatesXY = targetCoordinatesXY;
+        gameBoard[currentCoordinatesXY[1]][currentCoordinatesXY[0]] = "P";
+    }
+
+    /**
+     * Verifies if the desired movement direction is valid (not blocked by a wall)
+     * before updating Pac-Man's direction. Note that this method does not handle full
+     * collision detection; it only validates the direction.
+     * 
+     * @param gameBoard       A 2D array representing the current state of the game board.
+     *                        This includes walls, ghosts, Pac-Man, food, and other elements.
+     * @param inputDirectionXY The desired movement direction (x, y) specified by the user.
+     * 
+     * @see #checkCollisionAndMove(String[][]) Handles Pac-Man's actual movement and collision detection.
+     * @see Game#keyPressed(java.awt.event.KeyEvent) Updates the movement direction based on user input.
+     */
+    public void verifyDirectionUpdate(String[][] gameBoard, int[] inputDirectionXY) {
+        if (!gameBoard[currentCoordinatesXY[1] + inputDirectionXY[1]][currentCoordinatesXY[0] + inputDirectionXY[0]].equals("W")) {
+            updateDirection(inputDirectionXY);
+        }
+    }
 }
